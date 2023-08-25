73ae0c4f9ff2184e3043573bacd6a854920173f8
JaimeAGonzalez <ja.nunezg@uniandes.edu.co>
2023-08-25 18:41:50 -0500

    update

--- cypress/e2e/secondTest.cy.js
+++ cypress/e2e/secondTest.cy.js
@@ -3,7 +3,7 @@
 it.only("Electric Guitar selection", ()=> {
   // elements
   const desireField = 'Electric'
-  const randomElement = getRandomInt(0, 11);
+  const randomElement = Math.floor(Math.random() * 11)
 
   // Electric guitars
   cy.visit("https://reverb.com/c/electric-guitars/")
@@ -11,12 +11,12 @@ it.only("Electric Guitar selection", ()=> {
   
   // Choose
   cy.get(".csp-square-card__titleblock").then(element => {
-    cy.wrap(element).should("include.text", searchKeys)
+    cy.wrap(element).should("include.text", desireField)
 })
 
   // Chose random one
   cy.get(".csp-square-card__titleblock").eq(randomElement).click()
   cy.get(".csp2-header__image").should('be.visible')
-  cy.get("span[itemprop='name']").first().should('contain.text', randomElement)
+  cy.get("span[itemprop='name']").first().should('contain.text', desireField)
 }
 )
